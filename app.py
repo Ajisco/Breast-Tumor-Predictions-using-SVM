fe0c7fd5081157c157352c5805f996be652250df
Abdulquddus Ajibade <ajiscomorac@gmail.com>
2022-02-20 17:59:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def index():
         
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
     
     
     
