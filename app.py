4ff925ec6f38b096196549f4f4b83fd24a6e65d6
Abdulquddus Ajibade <ajiscomorac@gmail.com>
2022-02-20 13:54:51 +0100

    adding files

---
+++ app.py
@@ -0,0 +1,37 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+model= pickle.load(open('breast_flask.pkl', 'rb'))
+
+app= Flask(__name__)
+
+#Train model
+@app.route('/')
+def man():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods= ['POST'])
+def index():
+    data1= request.form['a']
+    data2= request.form['b']
+    data3= request.form['c']
+    data4= request.form['d']
+    data5= request.form['e']
+    data6= request.form['f']
+    data7= request.form['g']
+    data8= request.form['h']
+    arr = np.array([[data1,data2,data3,data4,data5,data6,data7,data8]])
+    pred= model.predict(arr)
+    return render_template('after.html', data=pred)
+        
+
+if __name__ == '__main__':
+    app.run(debug= True, use_reloader=False)
+    app.config['TEMPLATES_AUTO_RELOAD']=False
+    
+    
+     
+        
+        
